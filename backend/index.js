1ecf85ce71590568974379dc68d802792dc9c2b1
tthieu22 <tthieu.dev.02@gmail.com>
2025-06-21 08:19:10 +0700

    create, cancel, getbooking , get user booking , approve booking, delete booking

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connection = require("./src/config/database");
 const routerRoom = require("./src/router/routeRoom");
-
+const routerBooking = require("./src/router/routeBooking");
 dotenv.config();
 
 const app = express();
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/", routerRoom);
-
+app.use("/api/booking", routerBooking);
 (async () => {
   try {
     await connection();
